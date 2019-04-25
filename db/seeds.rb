5a83bbf9d98af3b2aa01de5c1ad679fe924b0bab
zalachenka <saliou.armony@gmail.com>
2019-04-25 19:30:18 +0200

    modif'

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,26 +4,32 @@ City.destroy_all
 Patient.destroy_all
 Specialty.destroy_all
 Appointment.destroy_all
+
 10.times do
 	city = City.create!(name: Faker::Nation.capital_city)
 end
-puts "10 fake cities created"
+puts "10 cities generated"
+
 10.times do
 	doc = Doctor.create!(first_name: Faker::Name.unique.first_name, last_name: Faker::Name.unique.last_name, zip_code: Faker::Address.zip_code, city_id: City.all.sample.id )
 end
 puts "10 fake docs created"
+
 10.times do
 	pat = Patient.create!(first_name: Faker::Name.unique.first_name, last_name: Faker::Name.unique.last_name, city_id: City.all.sample.id )
 end
 puts "10 fake patients created"
+
 10.times do
 	spe = Specialty.create!(name: ["Neurology", "Psychology", "Oncology", "Cardiology", "Radiology", "Obstetrics", "Pediatry", "General Medicine", "Urology", "Ophtalmology"].sample, doctor_id: Doctor.all.sample.id)
 end
 puts "10 fake specialties created"
+
 10.times do
 	rdv = Appointment.create!(date: Faker::Date.forward(23), city_id: City.all.sample.id, doctor_id: Doctor.all.sample.id, patient_id: Patient.all.sample.id)
 end
 puts "10 fake appointments created"
+
 10.times do
 	link = LinkSpeDoc.create!(doctor_id: Doctor.all.sample.id, specialty_id: Specialty.all.sample.id)
 end
